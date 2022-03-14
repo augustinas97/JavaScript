8af779481a2e8c3c4ddd12ac5f5b88c8e102c75f
augustinas9701~ <parasyk.man1234@gmail.com>
2022-03-14 19:36:58 +0200

    javascriptas

---
+++ index.js
@@ -0,0 +1,55 @@
+//let data1 = Math.floor (Math.random) *25
+//console.log
+
+//let lapas = prompt ('Iveskite suma')
+//console.log (typeoff)
+
+
+
+
+//console.log ('hello world');
+
+
+//let text1 = 'labas'
+//let text2 = 'vakaras'
+//let result = arr2.concat(arr2)
+//console.log
+// skaiciai
+
+
+
+//let name = 'vardenis';
+//let otherName = 'mykolas';
+//let GimimoMetai = 1997;
+//let dabartiniaiMetai = 2022;
+//let atsakymas = `${name} ${otherName} man yra ${dabartiniaiMetai - GimimoMetai}`;
+//console.log (atsakymas);
+
+if (data <20)
+
+
+
+
+
+
+
+
+
+let data = 2;
+if ( data < 2){
+    console .log(true)
+}
+else {
+    ( data > 2)
+    console.log(false)
+} else {
+    console.log('hi')
+}
+
+
+
+
+
+
+
+
