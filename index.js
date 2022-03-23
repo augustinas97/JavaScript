94093bddd9d263e0b09d677f71005a047b8003f9
augustinas9701~ <parasyk.man1234@gmail.com>
2022-03-23 21:21:07 +0200

    naujas

--- index.js
+++ index.js
@@ -143,14 +143,14 @@
 
 
 // Uzduotis
-const numbersArr = [2, 45, 3, 67, 34, 567, 34, 345, 123, 593]; 
-let didziausiasSkaicius = 0;
-for (let i = 0; i <= numbersArr.length - 1; i++) {
-  if(didziausiasSkaicius < numbersArr[i]) {
-    didziausiasSkaicius = numbersArr[i]
-  }
-}
-console.log(didziausiasSkaicius)
+//const numbersArr = [2, 45, 3, 67, 34, 567, 34, 345, 123, 593]; 
+//let didziausiasSkaicius = 0;
+//for (let i = 0; i <= numbersArr.length - 1; i++) {
+  //if(didziausiasSkaicius < numbersArr[i]) {
+    //didziausiasSkaicius = numbersArr[i]
+  //}
+//}
+//console.log(didziausiasSkaicius)
 // naudojantis for loop'u surasti didziausia skaiciu
 
 // Nested loops
@@ -304,36 +304,36 @@ console.log(didziausiasSkaicius)
 
 //sudetiVisusMasyvoSkaicius(sudetiVisusSkaicius)
 
-function kartotiniai(startinisSkaicius, galinisSkaicius){
+//function kartotiniai(startinisSkaicius, galinisSkaicius){
   // const skaiciai = Array.from(Array(100).keys());
-  const skaiciai = [];  
-  const trijuKartotiniai = [];
-  const penkiuKartotiniai = [];
-  const trijuIrPenkiuKartotiniai = [];
-  for (let i = startinisSkaicius; i <= galinisSkaicius; i++){
-    skaiciai.push(i)
-  }
+ // const skaiciai = [];  
+  //const trijuKartotiniai = [];
+  //const penkiuKartotiniai = [];
+  //const trijuIrPenkiuKartotiniai = [];
+  //for (let i = startinisSkaicius; i <= galinisSkaicius; i++){
+    //skaiciai.push(i)
+  //}
 
-  if (15 % 5 == 0) {
-    console.log('hi')
-  }
+  //if (15 % 5 == 0) {
+    //console.log('hi')
+  //}
 
-  for (let i = 0; i < skaiciai.length; i++) {
-    if (skaiciai[i] % 3 === 0 && skaiciai[i] % 5 === 0) {
-      trijuIrPenkiuKartotiniai.push(skaiciai[i])
+  //for (let i = 0; i < skaiciai.length; i++) {
+    //if (skaiciai[i] % 3 === 0 && skaiciai[i] % 5 === 0) {
+      //trijuIrPenkiuKartotiniai.push(skaiciai[i])
       // trijuKartotiniai.push(skaiciai[i]);
       // penkiuKartotiniai.push(skaiciai[i]);
-    } else if (skaiciai[i] % 3 === 0) {
-      trijuKartotiniai.push(skaiciai[i]);
-    } else if (skaiciai[i] % 5 === 0) {
-      penkiuKartotiniai.push(skaiciai[i]);
-    } 
-  }
-  console.log('triju', trijuKartotiniai)
-  console.log('penkiu', penkiuKartotiniai)
-  console.log('bendri', trijuIrPenkiuKartotiniai)
-}
-kartotiniai(0, 100)
+    //} else if (skaiciai[i] % 3 === 0) {
+     // trijuKartotiniai.push(skaiciai[i]);
+    //} else if (skaiciai[i] % 5 === 0) {
+      //penkiuKartotiniai.push(skaiciai[i]);
+    //} 
+ // }
+  //console.log('triju', trijuKartotiniai)
+  //console.log('penkiu', penkiuKartotiniai)
+  //console.log('bendri', trijuIrPenkiuKartotiniai)
+//}
+//kartotiniai(0, 100)
 
 // uzduotis parasyti funckija kuri startini ir galini skaiciu
 // musu atveju naudosime skaicius nuo 0 iki 100
@@ -347,23 +347,547 @@ kartotiniai(0, 100)
 // galiausiai i konsole isvesti tris masyvus kuriuose bus
 // triju, penkiu bei triju ir penkiu kartotiniai
 
-let str = 'labas';
+//let str = 'labas';
 
 // split above sentence into array of words, breaking at spaces
-let arr = str.split(" ");
+//let arr = str.split(" ");
 
 // iterate over the array, change the first character to uppercase for each word
 
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].charAt(0).toUpperCase(0) + arr[i].toUpperCase(4);
-}
+//for (let i = 0; i < arr.length; i++) {
+  //  arr[i] = arr[i].charAt(0).toUpperCase(0) + arr[i].toUpperCase(4);
+//}
 
 // join the words to form the string again
-str = arr.join(" ");
-
-console.log(str);
+//str = arr.join(" ");
 
 
 
 
 
+
+//console.log(str);
+
+//function capitalize(str) {
+  //const arrayFromString = str.split(' ');
+  //const newArr = []
+  //for (let i = 0; i < arrayFromString.length; i++) {
+    //let ilgisMinusVienas = arrayFromString[i].length - 1;
+    //if(arrayFromString[i].length === 1) {
+     // newArr.push(arrayFromString[i][0].toUpperCase());
+   // } else {
+  //    let capitalizedString = arrayFromString[i][0].toUpperCase() +
+ //     arrayFromString[i].slice(1, arrayFromString[i].length -2) + 
+ //     arrayFromString[i][ilgisMinusVienas].toUpperCase()
+ //    newArr.push(capitalizedString)
+ //   }
+ // }
+//}
+
+
+//const skaiciai = randomNumbers = [1, 22, 543, 2, 6, 12, 55, 86, 6, 9, 12, 31, 51, 92, 3];
+//skaiciai.sort(function(a, b) {
+  //return a - b;
+//});
+//console.log(skaiciai);
+
+//function IsvestiViduriniskaiciu(str) {
+
+  //let position;
+  //let length;
+
+  //if(str.length() % 2 == 1) {
+    //  position = str.length() / 2;
+      //length = 1;
+  //} else {
+    //  position = str.length() / 2 - 1;
+      //length = 2;
+  //}
+
+  //result = str.substring(position, position + length) 
+  
+
+//} 
+
+//IsvestiViduriniskaiciu('123456789')
+
+
+// Parasyti funkcija kuri suras vidurini zenkla Stringe ir isves ji i console
+// jei stringe yra nelyginis sibloliu skaicius, isvedame vidurini simboli
+// jei lyginis, du vidurinius
+
+//function grazinkViduriniSimboli(str){
+  //if (str.length % 2 === 0) { // lengt = 8
+   // lyginis - 1 = nelyginis 
+   // pirmas sprendimo budas
+    //let index = Math.floor((str.length - 1) / 2); // susirasti pirmo vidurinio stringo indexa
+    //console.log(str.slice(index, index + 2)) // pridedu 2 nes slice, neitraukia end indexo, todel + 1 netinka
+     // gaunasi kad cia yra slice(1, 1 + 2)
+     // antras sprendimo budas
+    //console.log(str.slice(str.length/2-1, str.length/2+1)); 
+ //} else {
+   ///         [ T e s s i ] .length() - grazins 5
+   ///         [ 0 1 2 3 4 ]
+   //let index = (str.length - 1) / 2;
+   //console.log(index); 
+   //console.log(str[index]) // str[2]
+  // }
+// }
+ 
+ //grazinkViduriniSimboli('Tessi')
+
+
+ 
+  //   const septyniuKartotinis =function(num) {
+   // if (num % 7 === 0) {
+   //   console.log(true)
+  //  } else {
+ //     console.log(false)
+  //  }
+  //}
+
+  //   septyniuKartotinis(76)
+
+ //
+// function addTen(func, num) {
+ // console.log(func);
+ /// console.log(num, 'num')
+  // musu atveju gaunasi, kad sita vieta yra
+  //console.log(func(num)) 
+   // konvertuojama i pridesiuDesimt(11)
+//}
+
+//function pridesiuDesimt(num) {
+ // console.log('pridesiuDesimt funkcija gauna skaiciu', num)
+ // return  10 + num
+//}
+// galiausiai gaunasi addTen(20, 11)
+// addTen(pridesiuDesimt(10), 11)
+//addTen(pridesiuDesimt, 11)
+
+// uzduotis suskikurti objekta kvadrato
+// sukurti du metodus (plotas ir perimetras) kurie priims krastines ilgi
+// kvadrato.plotas(10)  - grazins 100
+// kvadrato.perimetras(10) - 40
+
+
+//const knygosApieProgramavima = [
+  {
+ //   title: "The Pragmatic Programmer",
+  //  author: 'David Thomas',
+  //  goodReadsRating: 4.33,
+  //  numberOfRatings: 18579
+  //},
+  //{
+   // title: "Clean Code",
+    //author: 'Robert C. Martin',
+   // goodReadsRating: 4.39,
+   /// numberOfRatings: 18094
+  //},
+  //{
+   // title: "The Clean Coder",
+   // author: 'Robert C. Martin',
+   // goodReadsRating: 4.28,
+   // numberOfRatings: 7459
+  //},
+  //{
+   // title: "The Effective Engineer",
+   // author: 'Edmond Lau',
+   // goodReadsRating: 4.28,
+   // numberOfRatings: 1401
+  //},
+  ///{
+    //title: "Eloquent JavaScript",
+    //author: 'Marijn Haverbeke',
+    //goodReadsRating: 4.15,
+    //numberOfRatings: 2680
+  //}
+//]
+
+
+//const oldSyntax = {
+  //boom: 14,
+ // vardas: 'Jonas',
+ // pridekDvylika: function(item) {
+ //   console.log(item + this.boom)
+ // },
+ // kitas: function(){
+//    console.log('labas')
+  //}
+//}
+//oldSyntax.pridekDvylika(12) // situo atveju this ieskau
+// oldSyntax viduje
+//let newAdd12 = oldSyntax.pridekDvylika;
+// yra tas pats kas 
+// let newAdd12 = function(item) {
+  //   console.log(item + this.boom)
+  // },
+
+//newAdd12(8)
+
+// This priklauso nuo to kaip buvo iskviesta funkcija 
+// oldSyntax.pridekDvylika(8) // this priklausys nuo to kas kviecia funkcija
+// siuo atveju funkcija kviecia odlSyntax -> todel this refferins i oldSyntax vidu
+
+
+let myImg = document.querySelector('img');
+let myH1 = document.querySelector('h1')
+let container = document.getElementById('container')
+container.style.width = '300px'
+container.style.height = '300px'
+container.style.border = '2px solid gray'
+myImg.style.borderRadius = '50%';
+myImg.style.width = '70%';
+myImg.style.margin = '0 auto'
+myH1.style.textAlign = 'center'
+
+
+const animals = [
+  {
+    type: 'fox',
+    wild: true
+  }, 
+  {
+    type: 'cat',
+    wild: false
+  }, 
+  {
+    type: 'dog',
+    wild: false
+  }, 
+  {
+    type: 'rabbit',
+    wild: true
+  }, 
+  {
+    type: 'giraffe',
+    wild: true
+  }, 
+  {
+    type: 'monkey',
+    wild: true
+  }, 
+]
+// sukurti nauja masyva wildAnimals, kuriame nebus naminiu gyvunu
+// isivaizduokite, kad nezinome cat ir dog indeksu, 
+// todel elementus primiausia surasti
+// 1 budas
+// function filterIfWild(animal){
+//   return animal.wild === true
+// }
+// const wildAnimals2 = animals.filter(filterIfWild)
+
+// 2 budas 
+// const wildAnimals2 = animals.filter(function(animal){
+//   return animal.wild === true
+// })
+const wildAnimals2 = animals.filter(animal => animal.wild === true)
+console.log(wildAnimals2)
+
+const darbai = ['paimti darbus is masyvo', 'parasyti funkcija', 
+'funckija turi skurti DOM elementus', 
+'tie elementai turi buti sudeti i tevini ul konteineri']
+const myNewH1 = document.createElement('h1');
+myNewH1.innerText = "Sveiki is JS";
+document.body.appendChild(myNewH1);
+const myUl = document.createElement("ul");
+
+function addListItemsToUl(arr) {
+
+  arr.forEach(item => {
+    let li = document.createElement('li');
+    li.innerText = item;
+    myUl.append(li)
+  })
+ document.body.appendChild(myUl)
+}
+
+addListItemsToUl(darbai)
+
+const mainContainer = document.createElement('div');
+
+function createSingleCard(arr) {
+  // step 1 pereiti per visus arr elementus
+  arr.forEach(element => {
+    // step 1.1 susikurti div elementa i kuri sudesime 
+  // likusius elementus
+    let cardDiv = document.createElement('div');
+     // step 2 kiekvienam elementui sukuriame img elementa
+     let cardImg = document.createElement('img');
+       // src galime praleisti prisidesime veliau kai zinosime 
+  // kokiu klasiu reikes stilizuoti nuotraukai ir 
+  // korteles turiniui
+
+  // step 3 sukurti h3 elementa vardui ir pavardei
+  let cardH3 = document.createElement('h3');
+  // prideti teksta i sukurta elementa
+  cardH3.innerText = element.name;
+    // step 4 miestas eian i P taga
+    let cardP = document.createElement('p')
+  // p tagui reikia teksto
+  cardP.innerText = element.address.city;
+    // step 5 sukurti a lementa
+  // prideti jam teksta 'Nuoroda i svetaine'
+  // prideti href su elemento website value
+
+  let cardA = document.createElement('a');
+  cardA.innerText = 'Nuoroda i svetaine'
+  cardA.href = element.website
+  // visus sukurtus elementus sudedame i step 1.1 
+  // metu sukurta div
+  }
+   // viska sudedame i mainCintainer 
+  );
+
+
+  let duomenysIsDB = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      photo: "https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496"
+        }
+      },
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets"
+      }
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      photo: "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+      username: "Antonette",
+      email: "Shanna@melissa.tv",
+      address: {
+        street: "Victor Plains",
+        suite: "Suite 879",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+        geo: {
+          lat: "-43.9509",
+          lng: "-34.4618"
+        }
+      },
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {
+        name: "Deckow-Crist",
+        catchPhrase: "Proactive didactic contingency",
+        bs: "synergize scalable supply-chains"
+      }
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      photo: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+      username: "Samantha",
+      email: "Nathan@yesenia.net",
+      address: {
+        street: "Douglas Extension",
+        suite: "Suite 847",
+        city: "McKenziehaven",
+        zipcode: "59590-4157",
+        geo: {
+          lat: "-68.6102",
+          lng: "-47.0653"
+        }
+      },
+      phone: "1-463-123-4447",
+      website: "ramiro.info",
+      company: {
+        name: "Romaguera-Jacobson",
+        catchPhrase: "Face to face bifurcated interface",
+        bs: "e-enable strategic applications"
+      }
+    },
+    {
+      id: 4,
+      name: "Patricia Lebsack",
+      photo: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1727&q=80",
+      username: "Karianne",
+      email: "Julianne.OConner@kory.org",
+      address: {
+        street: "Hoeger Mall",
+        suite: "Apt. 692",
+        city: "South Elvis",
+        zipcode: "53919-4257",
+        geo: {
+          lat: "29.4572",
+          lng: "-164.2990"
+        }
+      },
+      phone: "493-170-9623 x156",
+      website: "kale.biz",
+      company: {
+        name: "Robel-Corkery",
+        catchPhrase: "Multi-tiered zero tolerance productivity",
+        bs: "transition cutting-edge web services"
+      }
+    },
+    {
+      id: 5,
+      name: "Tom Dietrich",
+      username: "Tom",
+      photo: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      email: "TomDietr@annie.ca",
+      address: {
+        street: "Skiles Walks",
+        suite: "Suite 351",
+        city: "Roscoeview",
+        zipcode: "33263",
+        geo: {
+          lat: "-31.8129",
+          lng: "62.5342"
+        }
+      },
+      phone: "(254)954-1289",
+      website: "demarco.info",
+      company: {
+        name: "Keebler LLC",
+        catchPhrase: "User-centric fault-tolerant solution",
+        bs: "revolutionize end-to-end systems"
+      }
+    },
+  ];
+
+  // uzduotis 
+// sukurti darbuotoju sarara, kur korteleje butu darbuotojo
+// nuotrauka 
+// vardar ir pavarde
+// miestas 
+// nuoroda i jo svetaine
+// paspaudus nuoroda svetaine turetu atsidaryti naujame lange
+
+
+const mainContainer = document.createElement('div');
+
+function createSingleCard(arr) {
+  // step 1 pereiti per visus arr elementus
+  arr.forEach(element => {
+    // step 1.1 susikurti div elementa i kuri sudesime 
+  // likusius elementus
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('card')
+     // step 2 kiekvienam elementui sukuriame img elementa
+     let cardImg = document.createElement('img');
+     cardImg.src = element.photo;
+     cardImg.classList.add('photo')
+       // src galime praleisti prisidesime veliau kai zinosime 
+  // kokiu klasiu reikes stilizuoti nuotraukai ir 
+  // korteles turiniui
+  // step 3 sukurti h3 elementa vardui ir pavardei
+  let cardH3 = document.createElement('h3');
+  // prideti teksta i sukurta elementa
+  cardH3.innerText = element.name;
+    // step 4 miestas eian i P taga
+    let cardP = document.createElement('p')
+  // p tagui reikia teksto
+  cardP.innerText = element.address.city;
+    // step 5 sukurti a lementa
+  // prideti jam teksta 'Nuoroda i svetaine'
+  // prideti href su elemento website value
+  let cardA = document.createElement('a');
+  cardA.innerText = 'Nuoroda i svetaine'
+  cardA.href = element.website
+  // visus sukurtus elementus sudedame i step 1.1 
+  // metu sukurta div
+  cardDiv.appendChild(cardImg);
+  cardDiv.appendChild(cardH3);
+  cardDiv.appendChild(cardP);
+  cardDiv.appendChild(cardA);
+  mainContainer.appendChild(cardDiv);
+  mainContainer.classList.add('container')
+
+  },
+  document.body.appendChild(mainContainer)
+   // viska sudedame i mainCintainer 
+  );
+}
+
+  createSingleCard(duomenysIsDB)
+
+  const mainContainer = document.createElement('div');
+mainContainer.classList.add('container')
+document.body.appendChild(mainContainer)
+
+function createSingleCard(arr) {
+  arr.forEach(element => {
+
+  let newCard = `
+    <div class="card">
+      <img class="photo" src="${element.photo}" alt="Profile pciture"> 
+      <h3>${element.name}</h3>
+      <p>${element.address.city}</p>
+    </div>
+  `
+  mainContainer.insertAdjacentHTML('beforeend', newCard)
+
+  },
+  );
+}
+
+  createSingleCard(duomenysIsDB)
+
+  function firstClick() {
+    console.log('First button was pressed');
+  }
+  
+  function secondClick() {
+    console.log('Second button was pressed');
+  }
+  
+  let firstBtn = document.createElement('button');
+  firstBtn.textContent = 'Pirmas';
+  document.body.appendChild(firstBtn);
+  firstBtn.addEventListener('click', firstClick);
+  
+  firstBtn.addEventListener('click', function() {
+    console.log('First button was pressed');
+  })
+  
+  firstBtn.addEventListener('click', () => console.log('Second button was pressed') ) 
+  
+  let secondButton = document.createElement('button');
+  secondBtn.textContent = 'Antras'
+  document.body.appendChild(secondBtn);
+  secondBtn.onclick = secondClick;
+
+
+
+
+  // uzduotis sukurti puslapi su mygtuku, kuri paspaudus pasikeitu
+// puslapio background spalva, kuri kas kart susigeneruose atsitiktine tvarka
+// naudosime RGB color
+// 1. Susikurti mygtuka + tekstas mygtukui + pridedam i body
+// 2. Apsirasyti funkcija kuri suras random spalva 
+// 2.1 reikes triju kintamuju, su random sugeneruotais skaiciais 0 - 255 / Math metodai
+// 2.2 kintamuosius turime perduoti i spalva rgb(x, y, z)
+// 2.3 dedam spalva ant body / inline stilius 
+// 3. susikuriam event listeneri onClick arba addEventListener
+
+let myCollorButton = document.createElement('button');
+myCollorButton.innerText = 'Keisti spalva';
+document.body.appendChild(myCollorButton);
+
+function sukurtiNaujaSpalva(){
+  let red = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  document.body.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`
+}
+
+// myCollorButton.onclick = sukurtiNaujaSpalva;
+myCollorButton.addEventListener('click', sukurtiNaujaSpalva)
